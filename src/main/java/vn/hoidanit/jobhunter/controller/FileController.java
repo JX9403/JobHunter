1e3fc26dd494c927b1c63f340d8a365e96795d6c
Doan Diem <diemdt9403@gmail.com>
2024-09-13 22:04:26 +0700

    video 102 download file

--- src/main/java/vn/hoidanit/jobhunter/controller/FileController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/FileController.java
@@ -1,12 +1,6 @@
 package vn.hoidanit.jobhunter.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
-import vn.hoidanit.jobhunter.domain.response.file.ResUploadFileDTO;
-import vn.hoidanit.jobhunter.service.FileService;
-import vn.hoidanit.jobhunter.util.error.StorageException;
-
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.time.Instant;
@@ -14,48 +8,88 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import vn.hoidanit.jobhunter.domain.response.file.ResUploadFileDTO;
+import vn.hoidanit.jobhunter.service.FileService;
+import vn.hoidanit.jobhunter.util.annotation.ApiMessage;
+import vn.hoidanit.jobhunter.util.error.StorageException;
 
 @RestController
 @RequestMapping("/api/v1")
 public class FileController {
 
-  private FileService fileService;
-  @Value("${hoidanit.upload-file.base-uri}")
-  private String baseURI;
+    @Value("${hoidanit.upload-file.base-uri}")
+    private String baseURI;
 
-  public FileController(FileService fileService) {
-    this.fileService = fileService;
-  }
+    private final FileService fileService;
 
-  @PostMapping("/files")
-  public ResponseEntity<ResUploadFileDTO> upload(@RequestParam("file") MultipartFile file,
-      @RequestParam("folder") String folder)
-      throws URISyntaxException, IOException, StorageException {
-
-    // validate
-    if (file.isEmpty() || file == null) {
-      throw new StorageException("File is empty!");
+    public FileController(FileService fileService) {
+        this.fileService = fileService;
     }
-    String fileName = file.getOriginalFilename();
-    List<String> allowedExtensions = Arrays.asList("pdf", "jpg", "jpeg", "png", "doc", "docx");
-    // create a directory if not exist
-    this.fileService.createUploadFolder(baseURI + folder);
 
-    boolean isValid = allowedExtensions.stream().anyMatch(item -> fileName.toLowerCase().endsWith(item));
+    @PostMapping("/files")
+    @ApiMessage("Upload single file")
+    public ResponseEntity<ResUploadFileDTO> upload(
+            @RequestParam(name = "file", required = false) MultipartFile file,
+            @RequestParam("folder") String folder
 
-    if (isValid == false) {
-      throw new StorageException("Invalid file extension!");
+    ) throws URISyntaxException, IOException, StorageException {
+        // skip validate
+        if (file == null || file.isEmpty()) {
+            throw new StorageException("File is empty. Please upload a file.");
+        }
+        String fileName = file.getOriginalFilename();
+        List<String> allowedExtensions = Arrays.asList("pdf", "jpg", "jpeg", "png", "doc", "docx");
+        boolean isValid = allowedExtensions.stream().anyMatch(item -> fileName.toLowerCase().endsWith(item));
+
+        if (!isValid) {
+            throw new StorageException("Invalid file extension. only allows " + allowedExtensions.toString());
+        }
+        // create a directory if not exist
+        this.fileService.createDirectory(baseURI + folder);
+
+        // store file
+        String uploadFile = this.fileService.store(file, folder);
+
+        ResUploadFileDTO res = new ResUploadFileDTO(uploadFile, Instant.now());
+
+        return ResponseEntity.ok().body(res);
     }
-    // store
 
-    String uploadFile = this.fileService.store(file, folder);
-    ResUploadFileDTO resUploadFileDTO = new ResUploadFileDTO(uploadFile, Instant.now());
+    @GetMapping("/files")
+    @ApiMessage("Download a file")
+    public ResponseEntity<Resource> download(
+            @RequestParam(name = "fileName", required = false) String fileName,
+            @RequestParam(name = "folder", required = false) String folder)
+            throws StorageException, URISyntaxException, FileNotFoundException {
+        if (fileName == null || folder == null) {
+            throw new StorageException("Missing required params : (fileName or folder) in query params.");
+        }
 
-    return ResponseEntity.status(HttpStatus.OK).body(resUploadFileDTO);
-  }
+        // check file exist (and not a directory)
+        long fileLength = this.fileService.getFileLength(fileName, folder);
+        if (fileLength == 0) {
+            throw new StorageException("File with name = " + fileName + " not found.");
+        }
+
+        // download a file
+        InputStreamResource resource = this.fileService.getResource(fileName, folder);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                .contentLength(fileLength)
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
+    }
 }
