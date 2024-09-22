24e51c5720791ce11b8d45064c2e1e0197ffe7a1
Doan Diem <diemdt9403@gmail.com>
2024-09-22 17:19:15 +0700

    video 124

--- src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
@@ -47,7 +47,7 @@ public class SecurityConfiguration {
         String[] whiteList = {
                 "/",
                 "/api/v1/auth/login", "/api/v1/auth/refresh", "/api/v1/auth/register",
-                "/storage/**",
+                "/storage/**", "/api/v1/email/**",
         };
 
         http
